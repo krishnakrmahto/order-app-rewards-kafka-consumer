f0c7d100bb3d5519923b598d9337bf9936edf9c2
krishnakrmahto97 <krish.thorcode@gmail.com>
2021-04-17 21:26:41 +0530

    consumer with @SendTo for sending reply message to the reply topic

--- src/main/java/com/course/kafka/broker/consumer/OrderConsumerThatReplies.java
+++ src/main/java/com/course/kafka/broker/consumer/OrderConsumerThatReplies.java
@@ -1,16 +1,20 @@
 package com.course.kafka.broker.consumer;
 
 import com.course.kafka.broker.message.OrderMessage;
+import com.course.kafka.broker.message.OrderReplyMessage;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Service;
 
-//@Service
+@Service
 @Slf4j
-public class OrderConsumer
+public class OrderConsumerThatReplies
 {
     @KafkaListener(topics = "t.commodity.order")
-    public void consume(ConsumerRecord<String, OrderMessage> consumerRecord)
+    @SendTo("t.commodity.order-reply")
+    public OrderReplyMessage consume(ConsumerRecord<String, OrderMessage> consumerRecord)
     {
         log.info("Consuming ConsumerRecord: {}", consumerRecord);
         int surpriseBonusPercentage = Integer.parseInt(new String(consumerRecord.headers().lastHeader("surpriseBonus").value()));
@@ -18,5 +22,11 @@ public class OrderConsumer
         OrderMessage message = consumerRecord.value();
         double surpriseBonusAmount = surpriseBonusPercentage * message.getPrice() * message.getQuantity() / 100.0;
         log.info("Surprise bonus amount for location {} is: {}", message.getOrderLocation(), surpriseBonusAmount);
+
+        return OrderReplyMessage.builder()
+                                .message("Bonus processed for orderNumber: " + message.getOrderNumber() + " at location: " +
+                                         message.getOrderLocation() + ", " + "bonusPercentage: " + surpriseBonusPercentage +
+                                         ", bonusAmount: " + surpriseBonusAmount)
+                                .build();
     }
 }
---
+++ src/main/java/com/course/kafka/broker/message/OrderReplyMessage.java
@@ -0,0 +1,15 @@
+package com.course.kafka.broker.message;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class OrderReplyMessage
+{
+    private String message;
+}
